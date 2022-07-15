09bbc02d2e6d85097d61773d18de34efaf48b0cc
Fabio Araújo <antoniofabio_araujo@outlook.com>
2022-07-14 20:07:30 -0400

    Repositories de cozinha e restaurante

---
+++ src/main/java/com/algafood/repository/CozinhaRepository.java
@@ -0,0 +1,9 @@
+package com.algafood.repository;
+
+import com.algafood.domain.model.Cozinha;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CozinhaRepository extends JpaRepository<Cozinha, Long> {
+}
---
+++ src/main/java/com/algafood/repository/RestauranteRepository.java
@@ -0,0 +1,9 @@
+package com.algafood.repository;
+
+import com.algafood.domain.model.Restaurante;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
+}
