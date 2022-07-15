5c333bcc45e798f5c6ed4065f6e145c0e65ec3bd
Fabio Araújo <antoniofabio_araujo@outlook.com>
2022-07-14 20:20:51 -0400

    Mapeamento das entidades estado, cidade e forma de pagamento

---
+++ src/main/java/com/algafood/domain/model/Cidade.java
@@ -0,0 +1,20 @@
+package com.algafood.domain.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cidade")
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Cidade {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    private String nome;
+}
