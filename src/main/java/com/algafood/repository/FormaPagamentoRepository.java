b7f18025ba59b40beb60b0255eae555baab022b3
Fabio Araújo <antoniofabio_araujo@outlook.com>
2022-07-14 20:24:19 -0400

    Repositories de estado, cidade e forma de pagamento

---
+++ src/main/java/com/algafood/repository/FormaPagamentoRepository.java
@@ -0,0 +1,7 @@
+package com.algafood.repository;
+
+import com.algafood.domain.model.FormaPagamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FormaPagamentoRepository extends JpaRepository<FormaPagamento, Long> {
+}
--- src/main/java/com/algafood/repository/RestauranteRepository.java
+++ src/main/java/com/algafood/repository/RestauranteRepository.java
@@ -2,8 +2,6 @@ package com.algafood.repository;
 
 import com.algafood.domain.model.Restaurante;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
 }
